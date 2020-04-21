4fe236294ea39dff97014a5b47a999a13cb0c8c7
peyman dehghan <peyman.dgn@outlook.com>
2020-04-21 19:46:46 +0430

    Add project files.

---
+++ RabittMqCore/ConsumeRabbitMQHostedService.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabittMqCore
+{
+    public class ConsumeRabbitMQHostedService : BackgroundService
+    {
+        private readonly ILogger _logger;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public ConsumeRabbitMQHostedService(ILoggerFactory loggerFactory)
+        {
+            this._logger = loggerFactory.CreateLogger<ConsumeRabbitMQHostedService>();
+            InitRabbitMQ();
+        }
+
+        private void InitRabbitMQ()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "37.152.191.66",
+                UserName = "Wistham",
+                Password = "Wistham@RoadEYE",
+                VirtualHost= "/",
+            };
+
+            // create connection  
+            _connection = factory.CreateConnection();
+
+            // create channel  
+            _channel = _connection.CreateModel();
+
+            _channel.ExchangeDeclare("demo.exchange.topic.dotnetcore", ExchangeType.Topic, true);
+            _channel.QueueDeclare("demo.exchange.topic.dotnetcore", true, false, false, null);
+            _channel.QueueBind("demo.exchange.topic.dotnetcore", "demo.exchange.topic.dotnetcore", "*.queue.durable.dotnetcore.#", null);
+            _channel.BasicQos(0, 1, false);
+
+            _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                // received message  
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+                // handle the received message  
+                HandleMessage(content);
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            consumer.Shutdown += OnConsumerShutdown;
+            consumer.Registered += OnConsumerRegistered;
+            consumer.Unregistered += OnConsumerUnregistered;
+            consumer.ConsumerCancelled += OnConsumerConsumerCancelled;
+
+            _channel.BasicConsume("demo.exchange.topic.dotnetcore", false, consumer);
+            return Task.CompletedTask;
+        }
+
+        private void HandleMessage(string content)
+        {
+            // we just print this message   
+            _logger.LogInformation($"consumer received {content}");
+        }
+
+        private void OnConsumerConsumerCancelled(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerUnregistered(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerRegistered(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerShutdown(object sender, ShutdownEventArgs e) { }
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e) { }
+
+        public override void Dispose()
+        {
+            _channel.Close();
+            _connection.Close();
+            base.Dispose();
+        }
+    }
+}
